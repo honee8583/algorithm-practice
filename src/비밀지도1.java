6cc6e12330eaa9f18c169e3fbc6b88b44e89a311
leedahoon <honee8583@naver.com>
2023-06-09 23:17:50 +0900

    [002] 프로그래머스 스쿨의 lv1 문제
    lv.1의 정답률 69%의 문제 도전
    
    arr1, arr2 배열을 각각 n길이의 이진수로 변환하는 메소드를 사용하여 이진수배열로 만들고 두 배열의 각 자리를 비교해서 하나라도 1이 존재할경우 '#'을 대입, 두개 모두 1이 존재하지 않을경우 공백을 추가하게 하여 해결했다.

---
+++ src/비밀지도1.java
@@ -0,0 +1,54 @@
+public class 비밀지도1 {
+    private static String[] toBinaryCode(int n, int[] arr) {
+        String[] str = new String[n];
+        for (int i = 0; i < arr.length; i++) {
+            String binary = Integer.toBinaryString(arr[i]);
+            while (binary.length() < n) {
+                binary = "0" + binary;
+            }
+            str[i] = binary;
+        }
+
+        return str;
+    }
+
+    public static String[] solution(int n, int[] arr1, int[] arr2) {
+
+        // 이진수로 변환
+        String[] str1 = toBinaryCode(n, arr1);
+
+        String[] str2 = toBinaryCode(n, arr2);
+
+        // 비교
+        String[] result = new String[n];
+        for (int i = 0; i < result.length; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            char[] cArr1 = str1[i].toCharArray();
+            char[] cArr2 = str2[i].toCharArray();
+
+            for (int j = 0; j < cArr1.length; j++) {
+                if (cArr1[j] == '1' || cArr2[j] == '1') {
+                    sb.append("#");
+                } else {
+                    sb.append(" ");
+                }
+            }
+
+            result[i] = sb.toString();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{9, 20, 28, 18, 11};
+        int[] arr2 = new int[]{30, 1, 21, 17, 28};
+//        System.out.println(solution(5, arr1, arr2));    // ["#####", "# # #", "### #", "# ##", "#####"]
+
+        String[] result = solution(5, arr1, arr2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + ", ");
+        }
+    }
+}
