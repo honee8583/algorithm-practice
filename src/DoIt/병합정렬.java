0405e230d154e5153a254e91ad8a23db2e4a797c
leedahoon <honee8583@naver.com>
2023-06-22 22:25:21 +0900

    [Doit!강의9 - 병합정렬]
    병합정렬의 핵심은 분할이 아닌 합병과정이다.
    분할은 합병할 인덱스의 범위를 1이 될때까지 쪼개는 과정일 뿐이다.
    합병과정에서 제대로된 정렬이 일어난다.
    합병하는 메소드에서는 조건들을 잘 명시해야 한다.
    좌측 서브배열의 시작 인덱스를 i, 우측 서브배열의 시작 인덱스를 j라고 했을 때 합병메소드에서 인자로 받은 left가 i, j는 mid + 1로 시작한다.
    i는 mid와 같아질때까지, j는 right와 같아질때까지 밑의 과정을 반복한다.
    i, j 모두 각각의 서브배열의 범위 안에 있다는 것은 두 서브배열에 아직 정렬할 원소가 남아있다는 뜻이다. 따라서 정렬한 결과를 담는 배열에 두 서브배열의 원소중 작은 원소먼저 넣어준다. 이 경우가 아니고 i인덱스만 해당 서브 배열범위안에 있다는 뜻은 우측 서브 배열의 원소는 정렬이 끝났고 좌측만 남았다는 뜻이므로 좌측 서브배열의 원소만 넣는다. 마지막으로 반대의 경우도 반대로 진행해준다.

---
+++ src/DoIt/병합정렬.java
@@ -0,0 +1,48 @@
+package DoIt;
+
+public class 병합정렬 {
+    public static void mergeSort(int[] arr,int[] result, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, result, left, mid);
+            mergeSort(arr, result, mid + 1, right);
+
+            merge(arr, result, left, right, mid);
+        }
+    }
+
+    public static void merge(int[] arr, int[] result, int left, int right, int mid) {
+        int i = left;
+        int j = mid + 1;
+        int idx = left;
+
+        // 중요한 부분
+        while (i <= mid || j <= right) {
+            if (i <= mid && j <= right) {   // 좌측, 우측 서브배열 모두 원소가 남은 상태인 경우
+                if (arr[i] <= arr[j]) {
+                    result[idx++] = arr[i++];
+                } else {
+                    result[idx++] = arr[j++];
+                }
+            } else if (i <= mid && j > right) { // 좌측 서브배열에만 원소가 남은 경우(오른쪽 서브배열은 정렬이 모두 완료된경우)
+                result[idx++] = arr[i++];
+            } else {                            // 우측 서브배열에만 원소가 남은 경우
+                result[idx++] = arr[j++];
+            }
+        }
+
+        for (int k = left; k <= right; k++) {
+            arr[k] = result[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{42,32,24,60,15,5,90,45};
+        int[] result = new int[arr.length];
+        mergeSort(arr, result, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
