3e05c9b2456ddcd72a641ef497a21e0da37ef021
leedahoon <honee8583@naver.com>
2023-07-27 15:05:07 +0900

    [백준 온라인 저지 2667번 - dfs/bfs] - 단지 번호 붙이기 시간 제한 : 1초 난이도 : 실버 1

---
+++ src/백준/B_2667.java
@@ -0,0 +1,75 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B_2667 {
+    static final int[] dx = {0, 0, 1, -1};
+    static final int[] dy = {1, -1, 0, 0};
+
+    static int cnt = 0;
+    static int number = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] matrix = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            String[] sArr = st.nextToken().split("");
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = Integer.parseInt(sArr[j]);
+            }
+        }
+
+        boolean[][] visited = new boolean[N][N];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                // 아직 방문하지 않았고 값이 1인 곳에서 dfs 시작
+                if (!visited[i][j] && matrix[i][j] == 1) {
+                    cnt = 0;    // 각 단지 개수 초기화
+                    number++;   // 단지 번호 증가
+                    dfs(matrix, visited, i, j);
+                    list.add(cnt);
+                }
+            }
+        }
+
+        Collections.sort(list);
+        System.out.println(list.size());
+        for (int a : list) {
+            System.out.println(a);
+        }
+
+        bf.close();
+    }
+
+    public static void dfs(int[][] matrix, boolean[][] visited, int i, int j) {
+        if (visited[i][j]) {
+            return;
+        }
+
+        visited[i][j] = true;
+        matrix[i][j] = number;
+        cnt++;
+
+        for (int k = 0; k < 4; k++) {
+            int x = i + dx[k];
+            int y = j + dy[k];
+
+            if (x >= 0 && x < matrix.length && y >= 0 && y < matrix.length) {
+                if (!visited[x][y] && matrix[x][y] == 1) {  // 다음 방문할 곳이 방문하지 않았고 값이 1인경우
+                    matrix[x][y] = number;
+
+                    dfs(matrix, visited, x, y);
+                }
+            }
+        }
+    }
+}
