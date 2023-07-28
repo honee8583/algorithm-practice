ee976b6e0957f33c3feb10ba76fd2746307e0c70
leedahoon <honee8583@naver.com>
2023-07-28 15:36:44 +0900

    [백준 온라인 저지 11724번 - dfs/bfs] - 연결 요소의 개수 시간 제한 : 3초 난이도 : 실버 2

---
+++ src/백준/B_11724.java
@@ -0,0 +1,59 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class B_11724 {
+    static int count = 0;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());   // 정점
+        int M = Integer.parseInt(st.nextToken());   // 간선
+
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(list, i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static void dfs(List<List<Integer>> list, int num) {
+        if (visited[num]) {
+            return;
+        }
+
+        visited[num] = true;
+
+        List<Integer> nextList = list.get(num);
+        for (int n : nextList) {
+            dfs(list, n);
+        }
+    }
+}
