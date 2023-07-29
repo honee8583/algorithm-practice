8b44fa51973605cda1c21353e36a7ab12c1de0cf
leedahoon <honee8583@naver.com>
2023-07-29 17:04:51 +0900

    [백준 온라인 저지 11725번 - dfs/bfs] - 트리의 부모 찾기 시간 제한 : 1초 난이도 : 실버 2

---
+++ src/백준/B_11725.java
@@ -0,0 +1,58 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B_11725 {
+    static boolean[] visited;
+    static Map<Integer, Integer> map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 정점의 개수
+
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(bf.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        bf.close();
+
+        map = new HashMap<>();
+        visited = new boolean[N + 1];
+
+        dfs(list, 1);
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(map.get(i));
+        }
+    }
+
+    public static void dfs(List<List<Integer>> list, int cur) {
+        if (visited[cur]) {
+            return;
+        }
+
+        visited[cur] = true;
+
+        List<Integer> nodeList = list.get(cur);
+        for (int n : nodeList) {
+            if (!visited[n]) {
+                map.put(n, cur);
+                dfs(list, n);
+            }
+        }
+    }
+}
