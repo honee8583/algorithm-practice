08743602027f33e244b68700617d00bf7c00b410
leedahoon <honee8583@naver.com>
2023-07-29 18:31:42 +0900

    [백준 온라인 저지 2644번 - dfs/bfs] - 촌수계산 시간 제한 : 1초 난이도 : 실버 2

---
+++ src/백준/B_2644.java
@@ -0,0 +1,69 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class B_2644 {
+    static int N;
+    static int M;
+    static int result = -1;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());   // 전체 사람의 수
+
+        st = new StringTokenizer(bf.readLine());
+        int a = Integer.parseInt(st.nextToken());   // 타겟1
+        int b = Integer.parseInt(st.nextToken());   // 타겟2
+
+        st = new StringTokenizer(bf.readLine());
+        M = Integer.parseInt(st.nextToken());       // 간선의 수
+
+        // 인접리스트
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list.get(x).add(y);
+            list.get(y).add(x);
+        }
+
+        dfs(list, a, b, 0);
+
+        System.out.println(result);
+    }
+
+    public static void dfs(List<List<Integer>> list, int cur, int target, int cnt) {
+        if (cur == target) {
+            result = cnt;
+            return;
+        }
+
+        if (visited[cur]) {
+            return;
+        }
+
+        visited[cur] = true;
+
+        List<Integer> targetList = list.get(cur);
+        for (int t : targetList) {
+            if (!visited[t]) {
+                dfs(list, t, target, cnt + 1);
+            }
+        }
+    }
+}
