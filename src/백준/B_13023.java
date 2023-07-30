3e4f82f0b449e7f68ce0b847a7a1e75488956dbe
leedahoon <honee8583@naver.com>
2023-07-30 15:27:51 +0900

    [백준 온라인 저지 13023번 - dfs/bfs] - ABCDE 시간 제한 : 2초 난이도 : 골드5
    * 2차원배열에서 영역의 개수를 구하는 문제가 아닌 그래프의 형태에서 특정 노드를 찾아가는 dfs 문제의 경우 방문배열에서 현재 방문한 노드를 다시 false로 바꿔 다음 재귀함수때 해당노드를 거쳐 다른 노드로 찾아갈 수 있도록 해줘야 한다.

---
+++ src/백준/B_13023.java
@@ -0,0 +1,74 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class B_13023 {
+    static boolean result = false;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 정점
+        int M = Integer.parseInt(st.nextToken());   // 간선
+
+        List<List<Integer>> fs = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            fs.add(new ArrayList<>());
+        }
+
+        // 인접리스트 구성
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            fs.get(a).add(b);
+            fs.get(b).add(a);
+        }
+
+        bf.close();
+
+        for (int i = 0; i < N; i++) {
+            visited = new boolean[N];   // 방문배열 초기화
+            dfs(fs, i, 1);  // 모든 정점에서 dfs 시작
+
+            if (result) {
+                break;
+            }
+        }
+
+        if (result) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+    public static void dfs(List<List<Integer>> list, int cur, int cnt) {
+        if (visited[cur]) {
+            return;
+        }
+
+        if (cnt >= 5) {
+            result = true;
+            return;
+        }
+
+        visited[cur] = true;
+
+        List<Integer> fsList = list.get(cur);
+        for (int next : fsList) {
+            if (!visited[next]) {
+                dfs(list, next, cnt + 1);
+                visited[next] = false;
+            }
+        }
+        visited[cur] = false;   // 핵심
+    }
+}
