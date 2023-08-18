4337b2419a0bf5c258fe0871b1d91cc207ca3e7f
leedahoon <honee8583@naver.com>
2023-08-18 10:54:59 +0900

    [백준 온라인 저지 2805번 - 이진탐색] 시간 제한 : 1초 난이도 : 실버 2
    - 이진탐색의 기본문제였지만, 대수롭지 않게 생각했던 자료형의 범위로 인해 계속 틀렸다.
    - int형은 +-21억쯤의 범위를 가지고 있지만 문제에서 가져가는 나무의 길이를 모두 합하게 될경우 그 범위를 넘어설 수 있기때문에 long 타입으로 지정해줘야 다른 테스트 케이스에서 통과를 할 수 있었다.

---
+++ src/백준/B_2805.java
@@ -0,0 +1,44 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_2805 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 나무의 개수
+        int M = Integer.parseInt(st.nextToken());   // 필요한 나무의 길이
+
+        int[] A = new int[N];   // 나무 길이 배열
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = Arrays.stream(A).max().getAsInt();
+        while (start <= end) {
+            int mid = (start + end) / 2;    // 톱날의 길이
+
+            long sum = 0;   // long 타입으로 선언해줘야 한다.
+            for(int tree: A) {
+                if (tree > mid) {
+                    sum += tree - mid;
+                }
+            }
+
+            // 만든 나무의 총 길이가 목표 나무의 길이보다 길 경우
+            if (sum < M) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(end);
+    }
+}
