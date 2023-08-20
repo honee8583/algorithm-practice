0ea226a81af18b6baf5bc0cc6be344b20cefdac2
leedahoon <honee8583@naver.com>
2023-08-20 19:03:54 +0900

    [백준 온라인 저지 2512번 - 이진탐색] - 에산 시간 제한 : 1초 난이도 : 실버 2
    - 이진탐색을 이용해서 푸는 문제이다.
    - 범위의 최솟값을 1, 최댓값을 받은 각 지방의 요청 예산중 최댓값으로 설정한다.
    - 탐색은 상한예산을 탐색하는 것이므로 각 지방의 요청예산과 현재 탐색중인 상한예산중 작은 값을 sum 변수에 더해 합을 구한다.
    - 만약 모두 합한 허용 예산이 최대 예산(M)보다 클경우 범위를 현재 탐색중인 값보다 작은 범위에서 탐색을 진행한다.반대의 경우에은 더 큰 범위에서 탐색을 진행한다.
    - 최댓값을 구하는 것이므로 break 없이 끝까지 탐색을 진행하고 start가 end를 넘어설 때의 end 값이 답이 된다.

---
+++ src/백준/B_2512.java
@@ -0,0 +1,44 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_2512 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        // 각 지방의 요청 예산(1 ~ 100000)
+        st = new StringTokenizer(bf.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(bf.readLine());
+        int M = Integer.parseInt(st.nextToken());   // 전체 국가 예산
+
+        int start = 1;
+        int end = Arrays.stream(A).max().getAsInt();
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                sum += Math.min(A[i], mid);
+            }
+
+            if (sum > M) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(end);
+    }
+}
