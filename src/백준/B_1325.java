3b4cd68b65b052fcc0b0e311caee918ba8eb25d0
leedahoon <honee8583@naver.com>
2023-08-27 15:02:59 +0900

    [백준 온라인 저지 1325번 - 그래프] - 효율적인 해킹 시간 제한 : 5초 난이도 : 실버 1
    - 그래프를 이용한 BFS 문제이다.
    
    내가 처음 푼 방식)
    - 처음에는 DFS문제인줄 알았지만 BFS를 사용하는 것이 옳았다.
    - 방문배열에 True/False를 넣는대신 지금까지 해당 루트로 거친 노드의 개수를 넣는 방식을 사용했다.
    - 시간초과를 해결하기 위해 현재 노드까지오기 위해 탐색한 노드 수가 다음 방문할 노드의 탐색 노드수보다 크거나 같을 경우 탐색을 이어가도록 했다.
    (이럴경우 문제는 풀리지 않았다)
    
    답안)
    - 방문배열과 탐색노드수를 저장한 배열을 따로 두었다.
    - 모든 노드에서 BFS를 시작하며 시작할 때마다 방문배열을 초기화 해준다.
    - BFS에서는 인접 리스트의 요소들을 방문하지 않은 경우 해당 요소들의 탐색을 진행하고 탐색노드수를 저장한 배열의 값을 1씩 증가시켜준다.

---
+++ src/백준/B_1325.java
@@ -0,0 +1,72 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B_1325 {
+    static int N;
+    static int M;
+
+    static ArrayList<Integer>[] list;    // 인접리스트
+    static boolean[] visited;               // 방문 및 신뢰관계 배열
+    static int[] count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());   // 컴퓨터의 개수
+        M = Integer.parseInt(st.nextToken());   // 신뢰관계의 개수
+
+        count = new int[N + 1];
+        list = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N + 1];
+            BFS(i);
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+        int max = Arrays.stream(count).max().getAsInt();
+        for (int i = 1; i < count.length; i++) {
+            if (count[i] == max) {
+                resultList.add(i);
+            }
+        }
+
+        Collections.sort(resultList);
+        for (int i : resultList) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void BFS(int computer) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(computer);
+        visited[computer] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int i : list[cur]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    count[i]++;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
