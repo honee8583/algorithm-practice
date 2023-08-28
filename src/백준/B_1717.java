a4a760b2459b969b3a01cf350800d648c0ec0e2c
leedahoon <honee8583@naver.com>
2023-08-28 16:03:43 +0900

    [백준 온라인 저지 1717번 - 유니온 파인드] - 집합의 표현 시간 제한 : 2초 난이도 : 골드 5
    - 처음에 배열의 값을 자신의 인덱스값으로 초기화한다.(자신만을 집합으로 하는 상태)
    - union 연산 : 두 집합을 합치는 연산이다. (값을 자신의 집합의 대표노드값으로 변경한다)
    - find 연산 : 두 집합이 같은 집합에 속하는지 확인하기 위해 집합의 최상위 부모 노드의 인덱스를 가져오는 연산이다.(이때 최하위 자식노드부터 최상위 부모 노드까지 거쳐가는 모든 노드의 값을 최상위 부모노드로 저장하면 나중에 부모노드를 찾는 연산에서 시간복잡도를 줄일 수 있다)

---
+++ src/백준/B_1717.java
@@ -0,0 +1,58 @@
+package 백준;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B_1717 {
+    static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // 배열 초기화
+        arr = new int[n + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        // 연산 입력(0: union, 1: find)
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == 0) {
+                union(b, c);
+            } else {
+                if (find(b) == find(c)) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+
+//        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void union(int a , int b) {
+        int parentA = find(a);  // a의 대표노드
+        int parentB = find(b);  // b의 대표노드
+
+        if (parentA < parentB) {
+            arr[parentB] = parentA;
+        } else if (parentA > parentB){
+            arr[parentA] = parentB;
+        }
+    }
+
+    public static int find(int num) {
+        if (arr[num] == num) {
+            return num;
+        }
+
+        return arr[num] = find(arr[num]);   // ** 중요 포인트: 대표노드를 찾기 위해 방문한 자식노드들의 값을 대표노드로 지정해준다. (시간복잡도 단축)
+    }
+}
