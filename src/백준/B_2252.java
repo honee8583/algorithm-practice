b7ba8bf2003d231df6c7b2fe1a4b90963c199fa0
leedahoon <honee8583@naver.com>
2023-08-30 15:36:25 +0900

    [백준 온라인 저지 2252번 - 위상정렬] - 줄 세우기 시간 제한 : 2초 난이도 : 골드 3
    - 위상정렬은 항상 유일한 값으로 정렬되지 않는다. 이 문제는 특정 학생이 특정 학생의 앞에만 있으면 되므로 위상정렬에 적합한 문제이다.
    
    - 앞에 존재해야 하는 학생이 뒤의 학생을 가리키게끔 인접리스트를 구성한다. 동시에 진입차수 배열을 설정한다.
    - 위상정렬을 시작하기 전에 큐를 생성하고 큐에 현재 진입차수 배열의 값이 0인 노드들을 넣어주고 시작한다.
    - 정렬을 하면서 현재 큐에서 poll한 값의 인접리스트를 순환하면서 인접 노드들의 진입차수 값을 1씩 증감시킨다.
    - 증감시키면서 0이된 노드들이 있으면 해당 노드들을 다시 큐에 삽입해준다.
    - 큐가 비어있지 않을때까지 반복해주고 그렇게 poll 한 순서들을 출력한다.

---
+++ src/백준/B_2252.java
@@ -0,0 +1,73 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B_2252 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 인접리스트
+        ArrayList<Integer>[] list = new ArrayList[N + 1];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        // 진입차수 배열
+        int[] arr = new int[N + 1];
+
+        boolean[] done = new boolean[N + 1];
+
+        // 인접리스트, 진입차수배열 구성
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+            arr[b]++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == 0 && !done[i]) {
+                queue.add(i);
+                done[i] = true;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            result.add(cur);
+
+            // 인접리스트 방문
+            for (int i : list[cur]) {
+                if (!done[i] && arr[i] > 0) {
+                    arr[i]--;
+                    if (arr[i] == 0) {
+                        queue.offer(i);
+                        done[i] = true;
+                    }
+                }
+            }
+
+//            for (int i = 1; i < arr.length; i++) {
+//                if (arr[i] == 0 && !done[i]) {
+//                    queue.add(i);
+//                    done[i] = true;
+//                }
+//            }
+        }
+
+        for (int r : result) {
+            System.out.print(r + " ");
+        }
+    }
+}
