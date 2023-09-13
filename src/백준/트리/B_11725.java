efd1e0514e6f01ded7e49f6d74a96512a0c84354
leedahoon <honee8583@naver.com>
2023-09-13 22:12:27 +0900

    [백준 온라인 저지 11725번 - 트리] - 트리의 부모 찾기 시간 제한 : 1초 난이도 : 실버 2
    - 트리의 부모노드의 값을 찾는 문제이다.
    - DFS 알고리즘을 이용해서 최상위노드를 1이라고 가정했을때 인접리스트를 통해 인접한 노드를 탐색해나가며 탐색한 노드의 부모노드를 전 노드로 저장한다.

---
+++ src/백준/트리/B_11725.java
@@ -0,0 +1,56 @@
+package 백준.트리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class B_11725 {
+    static int N;
+    static int[] parent;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+
+        // 연결리스트 생성
+        list = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // 연결리스트 구성
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        visited = new boolean[N + 1];
+        parent = new int[N + 1];
+
+        dfs(1);
+        parent[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(parent[i]);
+        }
+    }
+
+    public static void dfs(int node) {
+        visited[node] = true;
+
+        for (int next : list.get(node)) {
+            if (!visited[next]) {
+                parent[next] = node;
+                dfs(next);
+            }
+        }
+    }
+}
