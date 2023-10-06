a4288976b8b74ef660ab1106c051881ae85073cd
leedahoon <honee8583@naver.com>
2023-10-06 15:52:04 +0900

    [백준 온라인 저지 11050번 - 조합] - 이항계수1 시간 제한 : 1초 난이도 : 실버 2
    - 이항 계수(n, k)는 nCk를 구하라는 뜻이다.
    - nCk를 구하기 위해 2차원 배열 dp을 선언한다.
    - i개 중에서 1개를 선택하는 경우의수는 1 (dp[i][1] = 1), i개에서 1개도 선택하지 않는 경우의수는 1(dp[i][0] = 1), i개에서 i개를 선택하는 경우의수는 i (dp[i][i] = i) 인점을 고려해서 dp배열을 초기화한다.
    - 점화식을 이용해서 dp 배열의 나머지 부분들을 채워 나간다.

---
+++ src/백준/조합/B_11050.java
@@ -0,0 +1,31 @@
+package 백준.조합;
+
+import java.util.Scanner;
+
+public class B_11050 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        // 배열 초기화 (dp[i][1] = i, dp[i][0] = 1, dp[i][i] = 1)
+        int[][] dp = new int[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            dp[i][1] = i;
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+
+//        System.out.println(Arrays.deepToString(dp));
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            }
+        }
+
+//        System.out.println(Arrays.deepToString(dp));
+
+        System.out.println(dp[N][K]);
+    }
+}
