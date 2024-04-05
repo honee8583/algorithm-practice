86a730546ae856e1a20728a37eb61b6e2d408a2b
honee8583 <honee8583@naver.com>
2024-04-06 08:36:04 +0900

    [백준 온라인 저지 11659번, 11660번 - 구간합] - 재풀이

--- src/백준/구간합/B_11659.java
+++ src/백준/구간합/B_11659.java
@@ -3,41 +3,27 @@ package 백준.구간합;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B_11659 {
-    static int[] A;
-    static int[] B;
-
     public static void main(String[] args) throws IOException {
+        // BufferedReader가 Scanner보다 시간이 훨씬 짧게 걸림
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        A = new int[N];
-        B = new int[N + 1];
         st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-            B[i + 1] = B[i] + A[i];
+        int[] S = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
         }
 
-        for (int k = 0; k < M; k++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-
-            if (i == j) {
-                System.out.println(A[i - 1]);
-            } else {
-                System.out.println(getSum(i - 1, j));
-            }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(S[b] - S[a - 1]);
         }
-    }
-
-    static int getSum(int i, int j) {
-        return B[j] - B[i];
     }
 }
--- src/백준/구간합/B_11660.java
+++ src/백준/구간합/B_11660.java
@@ -13,7 +13,6 @@ public class B_11660 {
         int M = Integer.parseInt(st.nextToken());
 
         int[][] A = new int[N + 1][N + 1];
-        int[][] dp = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(bf.readLine());
             for (int j = 1; j <= N; j++) {
@@ -21,25 +20,21 @@ public class B_11660 {
             }
         }
 
+        int[][] S = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
-            dp[1][i] = dp[1][i - 1] + A[1][i];
-            dp[i][1] = dp[i - 1][1] + A[i][1];
-        }
-
-        for (int i = 2; i <= N; i++) {
-            for (int j = 2; j <= N; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + A[i][j];
+            for (int j = 1; j <= N; j++) {
+                S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + A[i][j];
             }
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
             int x1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            System.out.println(dp[y1][y2] - dp[y1][x2 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][x2 - 1]);
+            System.out.println(S[x2][y2] - S[x2][y1 - 1] - S[x1- 1][y2] + S[x1 - 1][y1 - 1]);
         }
     }
 }
