7e81374014e196990453859a72217965e0ef1b18
honee8583 <honee8583@naver.com>
2024-04-06 17:21:07 +0900

    [백준 온라인 저지 1012번 - DFS/BFS] - 재풀이

--- src/백준/그래프/DFS_BFS/B_1012.java
+++ src/백준/그래프/DFS_BFS/B_1012.java
@@ -1,75 +1,71 @@
 package 백준.그래프.DFS_BFS;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B_1012 {
-    static final int[] dx = {0, 0, -1, 1};
-    static final int[] dy = {1, -1, 0, 0};
+    static BufferedReader bf;
 
-    static int[][] matrix;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static int M, N, K;
+    static int[][] arr;
     static boolean[][] visited;
-    static int cnt;
-    static int T;
-    static int M;
-    static int N;
-    static int K;
+    static int answer;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        T = Integer.parseInt(st.nextToken());   // 테스트 케이스 수
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
 
         for (int i = 0; i < T; i++) {
-            cnt = 0;
-
-            st = new StringTokenizer(bf.readLine(), " ");
-            M = Integer.parseInt(st.nextToken());   // 가로
-            N = Integer.parseInt(st.nextToken());   // 세로
-            K = Integer.parseInt(st.nextToken());   // 심어져있는 배추의 개수
-
-            // 배추가 있는곳에 1 삽입
-            matrix = new int[N][M];
-            for (int k = 0; k < K; k++) {
-                st = new StringTokenizer(bf.readLine(), " ");
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-//                System.out.println("(" + a + ", " + b + ")");
-
-                matrix[b][a] = 1;
-            }
-
-            visited = new boolean[N][M];
-
-            for (int x = 0; x < N; x++) {
-                for (int y = 0; y < M; y++) {
-                    if (matrix[x][y] == 1 && !visited[x][y]) {
-                        dfs(x, y);
-                        cnt++;
-                    }
-                }
-            }
-
-            System.out.println(cnt);
+            solution();
         }
     }
 
-    public static void dfs(int i, int j) {
-        if (visited[i][j]) {
-            return;
+    public static void solution() throws IOException {
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        M = Integer.parseInt(st.nextToken());   // 가로길이
+        N = Integer.parseInt(st.nextToken());   // 세로길이
+        K = Integer.parseInt(st.nextToken());   // 배추개수
+
+        arr = new int[N][M];
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            arr[x][y] = 1;
         }
 
-        visited[i][j] = true;
+        answer = 0;
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] == 1 && !visited[i][j]) {
+                    DFS(i, j);
+                    answer++;
+                }
+            }
+        }
 
-        for (int k = 0; k < 4; k++) {
-            int x = i + dx[k];
-            int y = j + dy[k];
+        System.out.println(answer);
+    }
 
-            if (x >= 0 && x < N && y >= 0 && y < M) {
-                if (matrix[x][y] == 1 && !visited[x][y]) {
-                    dfs(x, y);
+    public static void DFS(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+
+            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M) {
+                if (!visited[nextX][nextY] && arr[nextX][nextY] == 1) {
+                    DFS(nextX, nextY);
                 }
             }
         }
