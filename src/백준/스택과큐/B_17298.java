ffa06c2c87e9f3510a27f50171cc0e539f6d932f
honee8583 <honee8583@naver.com>
2024-04-10 15:07:32 +0900

    [백준 온라인 저지 17298번 - 스택과 큐] - 재풀이

--- src/백준/스택과큐/B_17298.java
+++ src/백준/스택과큐/B_17298.java
@@ -6,38 +6,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class B_17298 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
 
-        int[] A = new int[N];
-        int[] result = new int[N];
-        String[] str = bf.readLine().split(" ");
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(str[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int[] answer = new int[N];
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
-        for (int i = 1; i < N; i++) {
-            // 스택이 비어있지 않고 현재 수열이 스택 top 인덱스가 가리키는 수열보다 클경우
-            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
-                result[stack.pop()] = A[i];
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                answer[stack.pop()] = arr[i];
             }
             stack.push(i);
         }
 
         while (!stack.isEmpty()) {
-            result[stack.pop()] = -1;
+            answer[stack.pop()] = -1;
         }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < N; i++) {
-            bw.write(result[i] + " ");
+            bw.write(answer[i] + " ");
         }
-
         bw.write("\n");
         bw.flush();
     }
