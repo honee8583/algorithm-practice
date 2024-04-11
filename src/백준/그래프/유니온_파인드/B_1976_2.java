de213c638eef300ba686da8ec38ec3dd0de628fa
honee8583 <honee8583@naver.com>
2024-04-11 17:12:59 +0900

    [백준 온라인 저지 1976번 - 유니온 파인드] - 재풀이

--- src/백준/그래프/유니온_파인드/B_1976_2.java
+++ src/백준/그래프/유니온_파인드/B_1976_2.java
@@ -6,70 +6,71 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B_1976_2 {
-    static int N, M;
-    static int[] route, parent;
-    static int[][] matrix;
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-
-        route = new int[M + 1];
-        matrix = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                int connection = Integer.parseInt(st.nextToken());
-                matrix[i][j] = connection;
-            }
-        }
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= M; i++) {
-            route[i] = Integer.parseInt(st.nextToken());
-        }
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());    // 도시의 수
+        int M = Integer.parseInt(bf.readLine());    // 여행계획에 속한 도시의 수
 
         parent = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             parent[i] = i;
         }
 
-        // 연결되어 있을 경우 union
+        StringTokenizer st = null;
         for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(bf.readLine());
             for (int j = 1; j <= N; j++) {
-                if (matrix[i][j] == 1) {
-                    union(i, j);
+                int idx = Integer.parseInt(st.nextToken());
+                if (idx == 1) {
+                    if (find(i) != find(j)) {
+                        union(i, j);
+                    }
                 }
             }
         }
 
-        // 여행경로의 최상위노드가 모두 같을 경우 'Yes', 같지 않을 경우 'No'
-        int start = find(route[1]);
-        for (int i = 2; i < route.length; i++) {
-            if (start != find(route[i])) {
-                System.out.println("NO");
-                return;
+        st = new StringTokenizer(bf.readLine());
+        int[] travel = new int[M + 1];
+        for (int i = 1; i <= M; i++) {
+            travel[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 모든 여행 경로의 루트가 같은지 확인
+        boolean flag = true;
+        int answer = find(travel[1]);
+        for (int i = 2; i <= M; i++) {
+            if (answer != find(travel[i])) {
+                flag = false;
+                break;
             }
         }
-        System.out.println("YES");
-    }
 
-    public static void union(int startPoint, int endPoint) {
-        startPoint = find(startPoint);
-        endPoint = find(endPoint);
-
-        if (startPoint != endPoint) {
-            parent[endPoint] = startPoint;
+        if (!flag) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
         }
     }
 
-    public static int find(int point) {
-        if (parent[point] == point) {
-            return point;
+    public static void union(int a, int b) {
+        if (parent[a] != parent[b]) {
+            a = find(a);
+            b = find(b);
         }
 
-        return parent[point] = find(parent[point]);
+        if (a < b) {
+            parent[a] = b;
+        } else if (b < a){
+            parent[b] = a;
+        }
+    }
+
+    public static int find(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = find(parent[node]);
     }
 }
