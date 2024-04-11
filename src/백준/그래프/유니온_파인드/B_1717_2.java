519f44746deb04babef687e62108c0bd0712a90b
honee8583 <honee8583@naver.com>
2024-04-11 16:02:39 +0900

    [백준 온라인 저지 1717번 - 유니온 파인드] - 재풀이

--- src/백준/그래프/유니온_파인드/B_1717_2.java
+++ src/백준/그래프/유니온_파인드/B_1717_2.java
@@ -3,59 +3,64 @@ package 백준.그래프.유니온_파인드;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B_1717_2 {
     static int[] parent;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 유니온-파인드 배열 구성
+        // 대표노드 배열 본인으로 초기화
         parent = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             parent[i] = i;
         }
 
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int calc = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(bf.readLine());
+            int check = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if (calc == 0) {
+            if (check == 0) {
                 // union
                 union(a, b);
+                System.out.println(Arrays.toString(parent));
             } else {
                 // find
-                if (find(a) != find(b)) {
-                    System.out.println("NO");
-                } else {
+                if (find(a) == find(b)) {
                     System.out.println("YES");
+                } else {
+                    System.out.println("NO");
                 }
             }
         }
     }
 
-    public static int find(int node) {
-        if (parent[node] == node) {
-            return node;
+    public static void union(int a, int b) {
+        // 각 노드의 부모 노드가 동일한지 확인
+        // 동일하지 않을 경우 대표노드를 찾아 연결
+        if (parent[a] != parent[b]) {
+            a = find(a);
+            b = find(b);
         }
 
-        return parent[node] = find(parent[node]);
+        if (a < b) {
+            parent[b] = a;
+        } else if (b < a){
+            parent[a] = b;
+        }
     }
 
-    public static void union(int a, int b) {
-        int parentA = find(a);
-        int parentB = find(b);
-
-        if (parentA < parentB) {
-            parent[parentB] = parentA;
-        } else {
-            parent[parentA] = parentB;
+    public static int find(int node) {
+        if (node == parent[node]) {
+            return node;
         }
+        return parent[node] = find(parent[node]);
     }
 }
