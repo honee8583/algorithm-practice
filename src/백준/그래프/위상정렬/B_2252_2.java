c5be04985eea44da25c7af213bcc6028ebd54939
honee8583 <honee8583@naver.com>
2024-04-12 22:04:35 +0900

    [백준 온라인 저지 2252번 - 위상 정렬] - 재풀이

--- src/백준/그래프/위상정렬/B_2252_2.java
+++ src/백준/그래프/위상정렬/B_2252_2.java
@@ -1,74 +1,61 @@
 package 백준.그래프.위상정렬;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class B_2252_2 {
-    static int N, M;
-    static ArrayList<Integer>[] adj;
-    static int[] inDegrees; // 진입차수 배열
-    static boolean[] visited;
-    static Queue<Integer> queue;
-    static ArrayList<Integer> result;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        adj = new ArrayList[N + 1];
-        for (int i = 1; i < N + 1; i++) {
-            adj[i] = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        inDegrees = new int[N + 1];
+        int[] D = new int[N + 1];   // 진입차수 배열
         for (int i = 0; i < M; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            st = new StringTokenizer(bf.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
 
-            adj[a].add(b);
-            inDegrees[b]++;
+            D[B] += 1;  // 진입차수 증가
+            adj.get(A).add(B);  // 인접리스트 추가
+            adj.get(B).add(A);  // 인접리스트 추가
         }
 
-        System.out.println(Arrays.toString(inDegrees));
-
-        visited = new boolean[N + 1];
-        result = new ArrayList<>();
-
-        updateDegrees();
-
-        for (int r : result) {
-            System.out.print(r + " ");
-        }
-    }
-
-    public static void updateDegrees() {
-        queue = new LinkedList<>();
-        insertQueue();
-
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            for (int next : adj[cur]) {
-                inDegrees[next]--;
-            }
-
-            insertQueue();
-        }
-    }
-
-    public static void insertQueue() {
+        boolean[] visited = new boolean[N + 1];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 1; i <= N; i++) {
-            if (inDegrees[i] == 0 && !visited[i]) {
-                queue.add(i);
-                result.add(i);
+            if (D[i] == 0) {    // 진입차수 값이 0일 경우
+                pq.add(i);
                 visited[i] = true;
-                break;
             }
-        }   // 큐 입력
-        System.out.println(queue);
+        }
+
+        while (!pq.isEmpty()) {
+            int cur = pq.poll();
+            System.out.print(cur + " ");
+
+            for (int next : adj.get(cur)) {
+                D[next]--;
+            }
+
+            for (int i = 1; i <= N; i++) {
+                if (D[i] == 0 && !visited[i]) { // 진입차수가 0이고 방문한적이 없을 경우 추가
+                    pq.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
     }
 }
