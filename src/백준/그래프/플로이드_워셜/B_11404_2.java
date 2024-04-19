82e25a95542df0a81f65d9911c828b51a3e2998e
honee8583 <honee8583@naver.com>
2024-04-20 00:44:03 +0900

    [백준 온라인 저지 11404번 - 플로이드 워셜] - 재풀이

--- src/백준/그래프/플로이드_워셜/B_11404_2.java
+++ src/백준/그래프/플로이드_워셜/B_11404_2.java
@@ -8,45 +8,45 @@ import java.util.StringTokenizer;
 public class B_11404_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());    // 도시의 개수
-        int m = Integer.parseInt(bf.readLine());    // 버스의 개수
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
 
         // 인접행렬 초기화
-        int[][] adj = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        long[][] bus = new long[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= N; j++) {
                 if (i == j) {
-                    adj[i][j] = 0;
+                    bus[i][j] = 0;
                 } else {
-                    adj[i][j] = 10000001;   // MAX_VALUE(21억~)로 설정하면 안됌 / long 타입으로 해도 안됌
+                    bus[i][j] = 10000001;
                 }
             }
         }
 
         // 인접행렬 구성
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            adj[a][b] = Math.min(adj[a][b], c);
+            bus[a][b] = Math.min(bus[a][b], c);
         }
 
-        for (int k = 1; k <= n; k++) {
-            for (int s = 1; s <= n; s++) {
-                for (int e = 1; e <= n; e++) {
-                    adj[s][e] = Math.min(adj[s][e], adj[s][k] + adj[k][e]);
+        for (int K = 1; K <= N; K++) {
+            for (int S = 1; S <= N; S++) {
+                for (int E = 1; E <= N; E++) {
+                    bus[S][E] = Math.min(bus[S][E], bus[S][K] + bus[K][E]);
                 }
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (adj[i][j] == 10000001) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (bus[i][j] == 10000001) {
                     System.out.print("0 ");
                 } else {
-                    System.out.print(adj[i][j] + " ");
+                    System.out.print(bus[i][j] + " ");
                 }
             }
             System.out.println();
