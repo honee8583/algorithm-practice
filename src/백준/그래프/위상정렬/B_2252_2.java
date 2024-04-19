253c640f067d0a591119f82eeeaaafd3546f5df2
honee8583 <honee8583@naver.com>
2024-04-20 00:34:14 +0900

    [백준 온라인 저지 2252번 - 위상정렬] - 주석 추가

--- src/백준/그래프/위상정렬/B_2252_2.java
+++ src/백준/그래프/위상정렬/B_2252_2.java
@@ -10,29 +10,31 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class B_2252_2 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        // 인접리스트 구성
         List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             adj.add(new ArrayList<>());
         }
 
-        int[] D = new int[N + 1];   // 진입차수 배열
+        // 진입차수 배열 구성
+        int[] D = new int[N + 1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            D[B] += 1;  // 진입차수 증가
+            D[B] += 1;          // 진입차수 증가
             adj.get(A).add(B);  // 인접리스트 추가
             adj.get(B).add(A);  // 인접리스트 추가
         }
 
+        // 방문배열 구성
         boolean[] visited = new boolean[N + 1];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 1; i <= N; i++) {
@@ -46,6 +48,7 @@ public class B_2252_2 {
             int cur = pq.poll();
             System.out.print(cur + " ");
 
+            // 방문후 진입차수 감소
             for (int next : adj.get(cur)) {
                 D[next]--;
             }
