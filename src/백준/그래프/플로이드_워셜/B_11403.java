876a5826c987177b56865cca2df4644893091592
honee8583 <honee8583@naver.com>
2024-04-19 15:12:39 +0900

    [백준 온라인 저지 11403번 - 플로이드 워셜]

--- src/백준/그래프/플로이드_워셜/B_11403.java
+++ src/백준/그래프/플로이드_워셜/B_11403.java
@@ -3,41 +3,35 @@ package 백준.그래프.플로이드_워셜;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class B_11403 {
-    static int N;
-    static int[][] distance;
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
+        int N = Integer.parseInt(bf.readLine());
 
-        // 인접행렬 초기화
-        distance = new int[N][N];
-        for (int i = 0; i < N; i++) {
+        // 인접행렬 구성
+        int[][] adj = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < N; j++) {
-                distance[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 1; j <= N; j++) {
+                adj[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 플로이드-워셜 알고리즘 수정
-        for (int k = 0; k < N; k++) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    // i와 j가 모두 k와 연결되어 있다면 업데이트한다.
-                    if (distance[i][k] == 1 && distance[k][j] == 1) {
-                        distance[i][j] = 1;
+        for (int K = 1; K <= N; K++) {
+            for (int S = 1; S <= N; S++) {
+                for (int E = 1; E <= N; E++) {
+                    if (adj[S][K] == 1 && adj[K][E] == 1) {
+                        adj[S][E] = 1;
                     }
                 }
             }
         }
 
-        // 출력
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(distance[i][j] + " ");
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.print(adj[i][j] + " ");
             }
             System.out.println();
         }
