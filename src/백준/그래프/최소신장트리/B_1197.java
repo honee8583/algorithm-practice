45fedebcdc4c997e539a7cd1fc1f9fba0ec86ab8
honee8583 <honee8583@naver.com>
2024-04-19 16:54:51 +0900

    [백준 온라인 저지 1197번 - 최소신장트리] - 재풀이

--- src/백준/그래프/최소신장트리/B_1197.java
+++ src/백준/그래프/최소신장트리/B_1197.java
@@ -7,85 +7,78 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class B_1197 {
-    static int V, E;
-    static int[] parent;
-
     public static class Edge implements Comparable<Edge> {
-        int node;
-        int next;
-        long weight;
+        int start;
+        int end;
+        int weight;
 
-        public Edge(int node, int next, long weight) {
-            this.node = node;
-            this.next = next;
+        public Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
             this.weight = weight;
         }
 
         @Override
         public int compareTo(Edge o) {
-            if (this.weight > o.weight) {
-                return 1;
-            } else {
-                return -1;
-            }
+            return this.weight - o.weight;
         }
     }
 
-    public static int find(int node) {
-        if (parent[node] == node) {
-            return node;
-        } else {
-            return parent[node] = find(parent[node]);
-        }
-    }
-
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        // 대표노드가 다를경우에만 병합
-        if (a != b) {
-            parent[b] = a;
-        }
-    }
+    static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        V = Integer.parseInt(st.nextToken());   // 정점의 개수
-        E = Integer.parseInt(st.nextToken());   // 간선의 개수
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        // 유니온 파인드 배열 초기화(인덱스값으로 초기화)
-        parent = new int[V + 1];
-        for (int i = 0; i <= V; i++) {
-            parent[i] = i;
-        }
-
-        // 에지리스트 생성(가중치 오름차순 정렬)
         PriorityQueue<Edge> pq = new PriorityQueue<>();
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(bf.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
 
-            pq.offer(new Edge(a, b, c));
+            pq.offer(new Edge(A, B, C));
         }
 
-        // 최소신장트리 구성
-        int num = 0;
-        int sum = 0;
-        while (num < V - 1) {   // 정점의 개수 - 1 만큼만 반복한다.(pq가 빌때까지가 아니다)
-            Edge cur = pq.poll();
+        parent = new int[V + 1];  // 유니온 파인드 배열
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
 
-            // 대표노드가 다를 경우에만 union연산 수행하면서 가중치 합 계산
-            if (find(cur.node) != find(cur.next)) {
-                union(cur.node, cur.next);
-                sum += cur.weight;
-                num++;
+        int N = 0;
+        int sum = 0;
+        while (!pq.isEmpty() && N != V - 1) {
+            Edge edge = pq.poll();
+            if (find(edge.start) != find(edge.end)) {
+                union(edge.start, edge.end);
+                N++;
+                sum += edge.weight;
             }
         }
 
         System.out.println(sum);
+    }
+
+    public static void union(int a, int b) {
+        if (parent[a] != parent[b]) {
+            a = find(a);
+            b = find(b);
+        }
+
+        if (a < b) {
+            parent[b] = a;
+        } else if (b < a){
+            parent[a] = b;
+        }
+    }
+
+    public static int find(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+
+        return parent[node] = find(parent[node]);
     }
 }
