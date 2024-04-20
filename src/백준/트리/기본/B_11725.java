69b7eafe2a7b95f9378e99c0d92b386dc618c027
honee8583 <honee8583@naver.com>
2024-04-20 20:36:07 +0900

    [백준 온라인 저지 11725번 - 트리] - 재풀이

--- src/백준/트리/기본/B_11725.java
+++ src/백준/트리/기본/B_11725.java
@@ -7,49 +7,40 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class B_11725 {
-    static int N;
+    static ArrayList<Integer>[] adj;
     static int[] parent;
-    static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> list;
-
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
+        int N = Integer.parseInt(bf.readLine());
 
-        // 연결리스트 생성
-        list = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            list.add(new ArrayList<>());
+        // 양방향 인접리스트
+        adj = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
         }
 
-        // 연결리스트 구성
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-            list.get(a).add(b);
-            list.get(b).add(a);
+            adj[S].add(E);
+            adj[E].add(S);
         }
 
-        visited = new boolean[N + 1];
         parent = new int[N + 1];
-
-        dfs(1);
-        parent[1] = 1;
+        DFS(1);
 
         for (int i = 2; i <= N; i++) {
             System.out.println(parent[i]);
         }
     }
 
-    public static void dfs(int node) {
-        visited[node] = true;
-
-        for (int next : list.get(node)) {
-            if (!visited[next]) {
-                parent[next] = node;
-                dfs(next);
+    public static void DFS(int cur) {
+        for (int next : adj[cur]) {
+            if (parent[next] == 0) {
+                parent[next] = cur;
+                DFS(next);
             }
         }
     }
