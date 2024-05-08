2eecebda87aed531ccbc348364a39c302ae12b64
honee8583 <honee8583@naver.com>
2024-05-08 16:24:09 +0900

    [백준 온라인 저지 B_1463 동적계획법] - 정수를 1로 만들기(재풀이)

--- src/백준/동적계획법/B_1463.java
+++ src/백준/동적계획법/B_1463.java
@@ -7,19 +7,14 @@ public class B_1463 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int[] dp = new int[N + 1];
-        dp[0] = 0;
-        dp[1] = 0;
+        int[] DP = new int[N + 1];
+        DP[1] = 0;
         for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            }
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
+            DP[i] = DP[i - 1] + 1;  // 1을 빼는 연산
+            if (i % 2 == 0) DP[i] = Math.min(DP[i], DP[i / 2] + 1); // 2로 나누는 연산
+            if (i % 3 == 0) DP[i] = Math.min(DP[i], DP[i / 3] + 1); // 3으로 나누는 연산
         }
 
-        System.out.println(dp[N]);
+        System.out.println(DP[N]);
     }
 }
