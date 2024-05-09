28a8769a7e0c4129a1f1ae6c165a480843c4d210
honee8583 <honee8583@naver.com>
2024-05-09 15:32:27 +0900

    [백준 온라인 저지 B_14501 동적계획법] - 퇴사 (재풀이)

--- src/백준/동적계획법/B_14501.java
+++ src/백준/동적계획법/B_14501.java
@@ -3,6 +3,7 @@ package 백준.동적계획법;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B_14501 {
@@ -10,24 +11,33 @@ public class B_14501 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
 
-        // 걸리는 시간배열, 수입배열 초기화
-        int[] T = new int[N + 1];
-        int[] P = new int[N + 1];
+        int[] T = new int[N + 1];   // 날짜 배열
+        int[] P = new int[N + 1];   // 금액 배열
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            T[i] = Integer.parseInt(st.nextToken());
-            P[i] = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            T[i] = t;
+            P[i] = p;
         }
 
-        int[] A = new int[N + 2];
-        for (int i = N; i >= 1; i--) {
-            if (i + T[i] > N + 1) { // 퇴사날짜를 넘기는 경우
-                A[i] = A[i + 1];
-            } else {    // 퇴사날짜를 넘기지 않는 경우
-                A[i] = Math.max(A[i + 1], A[i + T[i]] + P[i]);
+//        System.out.println(Arrays.toString(T));
+//        System.out.println(Arrays.toString(P));
+
+        int[] DP = new int[N + 2];  // i번째날부터 퇴사일까지 벌 수 있는 최대 금액
+        for (int i = N; i > 0; i--) {
+            // 퇴사일까지 못 끝내는 경우
+            if (i + T[i] > N + 1) {
+                DP[i] = DP[i + 1];
+            } else {
+                // 퇴사일안에 끝낼 수 있는 경우
+                // ** 다음날부터 벌 수 있는 최대 금액과 비교를 해야 한다.
+                DP[i] = Math.max(DP[i + 1], DP[i + T[i]] + P[i]);
             }
         }
 
-        System.out.println(A[1]);
+//        System.out.println(Arrays.toString(DP));
+        System.out.println(DP[1]);
     }
 }
