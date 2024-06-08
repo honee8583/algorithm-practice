dbbdc1d55d49713052ede0d39c21e4d03866f053
honee8583 <honee8583@naver.com>
2024-06-08 20:32:46 +0900

    [프로그래머스 - DFS/BFS] - 게임맵 최단거리

--- src/programmers/게임맵최단거리.java
+++ src/programmers/게임맵최단거리.java
@@ -4,51 +4,44 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class 게임맵최단거리 {
-    static final int[] dx = {0, 0, -1, 1};
-    static final int[] dy = {1, -1, 0, 0};
-    public static void solution(int[][] maps) {
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        int count = BFS(maps, visited,0, 0);
+    static final int[] dx = {-1, 1, 0, 0};
+    static final int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static int N, M;
+    private static int solution(int[][] maps) {
+        N = maps.length;
+        M = maps[0].length;
 
-        System.out.println(count);
+        visited = new boolean[N][M];
+        BFS(maps);
+        return !visited[N - 1][M - 1] ? -1 : maps[N - 1][M - 1];
     }
 
-    public static int BFS(int[][] maps, boolean[][] visited, int i, int j) {
-        int goalX = maps.length - 1;
-        int goalY = maps[0].length - 1;
+    private static void BFS(int[][] maps) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {0, 0});
+        visited[0][0] = true;
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{i, j});
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nextX = cur[0] + dx[i];
+                int nextY = cur[1] + dy[i];
 
-            for (int k = 0; k < 4; k++) {
-                // 다음좌표
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-
-                if (x >= 0 && y >= 0 && x < maps.length && y < maps[0].length) {
-                    if (maps[x][y] == 1) {
-                        queue.add(new int[]{x, y});
-                        maps[x][y] = maps[now[0]][now[1]] + 1;
-                        visited[x][y] = true;
+                if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < M) {
+                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
+                        q.add(new int[] {nextX, nextY});
+                        visited[nextX][nextY] = true;
+                        maps[nextX][nextY] = maps[cur[0]][cur[1]] + 1;
                     }
                 }
             }
         }
-
-        if (!visited[goalX][goalY]) {
-            return -1;
-        }
-
-        return maps[goalX][goalY];
     }
-    public static void main(String[] args) {
-        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
-        solution(maps); // 1
 
-        maps = new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
-        solution(maps); // -1
+    public static void main(String[] args) {
+        System.out.println(solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}}));
+        System.out.println(solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}}));
     }
 }
