7312428edb63726dc8d6bc567d61705f0fcaef66
honee8583 <honee8583@naver.com>
2024-08-09 15:33:58 +0900

    [백준 온라인 저지 - B_11055 DP] - 가장 큰 증가하는 부분 수열

---
+++ src/백준/동적계획법/B_11055.java
@@ -0,0 +1,38 @@
+package 백준.동적계획법;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_11055 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        dp[0] = arr[0];
+        for (int i = 1; i < N; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+
+        bw.write(Arrays.stream(dp).max().getAsInt() + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
