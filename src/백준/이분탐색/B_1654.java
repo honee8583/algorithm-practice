c12896b0f44f2d7b3d36bad1baecbb4c09cf471c
honee8583 <honee8583@naver.com>
2024-09-08 14:56:16 +0900

    [백준 온라인 저지 - B_1654 이분탐색] - 랜선 자르기

--- src/백준/이분탐색/B_1654.java
+++ src/백준/이분탐색/B_1654.java
@@ -1,46 +1,47 @@
 package 백준.이분탐색;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B_1654 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        int K = Integer.parseInt(st.nextToken());   // ~10,000(만)
-        int N = Integer.parseInt(st.nextToken());   // ~1000,000(백만)
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        int[] lines = new int[K];
+        int[] arr = new int[K];
         for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(bf.readLine());
-            lines[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        long result = -1;
-
+        long max = Integer.MIN_VALUE;
         long start = 1;
-        long end = Arrays.stream(lines).max().getAsInt();
+        long end = Arrays.stream(arr).max().getAsInt();
         while (start <= end) {
-            long mid = start + (end - start) / 2;
+            long mid = (start + end) / 2;
 
-            int sum = 0;    // 만들어지는 랜선의 개수
-            for (int i = 0; i < lines.length; i++) {
-                sum += lines[i] / mid;
+            long sum = 0;
+            for (int i = 0; i < K; i++) {
+                sum += arr[i] / mid;
             }
 
-            if (sum == N) {
-                result = Math.max(result, mid);
+            if (sum >= N) {
+                max = Math.max(max, mid);
                 start = mid + 1;
-            } else if (sum < N) {
-                end = mid - 1;
             } else {
-                start = mid + 1;
+                end = mid - 1;
             }
         }
-        result = Math.max(result, end);
-        System.out.println(result);
+
+        bw.write(max + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
