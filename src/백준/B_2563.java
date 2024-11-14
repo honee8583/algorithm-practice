45f85d5307c21821fa9ea4d4bb78f441c179a6cd
leedahoon <ekgns0508@gmail.com>
2024-11-14 14:29:17 +0900

    [백준 2563 구현] - 색종이

---
+++ src/백준/B_2563.java
@@ -0,0 +1,34 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_2563 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[100][100];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = y; j < y + 10; j++) {
+                for (int k = x; k < x + 10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        int result = Arrays.stream(arr)
+                .flatMapToInt(Arrays::stream)
+                .filter(i -> i == 1)
+                .sum();
+
+        System.out.println(result);
+    }
+}
