688a92e587920058d7f687239592b53692f8b33d
leedahoon <ekgns0508@gmail.com>
2024-11-21 18:51:54 +0900

    [백준 2564 구현] refactoring

--- src/백준/구현/B_2564.java
+++ src/백준/구현/B_2564.java
@@ -16,81 +16,41 @@ public class B_2564 {
         int height = Integer.parseInt(st.nextToken());
         int store = Integer.parseInt(br.readLine());
 
-        boolean[][] map = new boolean[height + 1][width + 1];
-        for (int i = 0; i < store; i++) {
+        int result = 0;
+        int total = (width + height) * 2;
+        int[] dists = new int[store];
+        for (int i = 0; i <= store; i++) {
             st = new StringTokenizer(br.readLine());
             int way = Integer.parseInt(st.nextToken());
             int distance = Integer.parseInt(st.nextToken());
 
-            if (way == 1) {
-                map[height][distance] = true;
-            } else if (way == 2) {
-                map[0][distance] = true;
-            } else if (way == 3) {
-                map[height - distance][0] = true;
+            int tmp = 0;
+            switch (way) {
+                case 1:
+                    tmp = distance;
+                    break;
+                case 2:
+                    tmp = width + height + width - distance;
+                    break;
+                case 3:
+                    tmp = width + height + width + height - distance;
+                    break;
+                case 4:
+                    tmp = width + distance;
+                    break;
+            };
+
+            if (i != store) {
+                dists[i] = tmp;
             } else {
-                map[height - distance][width] = true;
+                for (int j = 0; j < store; j++) {
+                    int diff = Math.abs(dists[j] - tmp);
+                    result += Math.min(diff, total - diff);
+                }
             }
         }
 
-        st = new StringTokenizer(br.readLine());
-        int way = Integer.parseInt(st.nextToken());
-        int distance = Integer.parseInt(st.nextToken());
-
-        // 시작위치
-        int x;
-        int y;
-        if (way == 1) {
-            x = height;
-            y = distance;
-        } else if (way == 2) {
-            x = 0;
-            y = distance;
-        } else if (way == 3) {
-            x = height - distance;
-            y = 0;
-        } else {
-            x = height - distance;
-            y = width;
-        }
-
-        int total = (width + height) * 2;
-        int sum = 0;
-        int cnt = 0;
-        int checkedStore = 0;
-        while (checkedStore < store) {
-            if (map[x][y]) {
-                // 상점을 만난 경우 최소거리 합산
-                sum += Math.min(cnt, total - cnt);
-                checkedStore++;
-            }
-
-            // 좌표 이동
-            if (x == 0) {
-                if (y == width) {
-                    x++;
-                } else {
-                    y++;
-                }
-            } else if (y == width) {
-                if (x == height) {
-                    y--;
-                } else {
-                    x++;
-                }
-            } else if (x == height) {
-                if (y == 0) {
-                    x--;
-                } else {
-                    y--;
-                }
-            } else {
-                x--;
-            }
-            cnt++;
-        }
-
-        bw.write(sum +"\n");
+        bw.write(result +"\n");
         bw.flush();
         br.close();
         bw.close();
