342a038fc3777c407b0fdab60b6e29a264ce4cac
honee8583 <93666531+honee8583@users.noreply.github.com>
2025-05-18 16:10:30 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14896 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        long[] A = new long[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int cnt = 0;
+        for (int k = 0; k < N; k++) {
+            long find = A[k];
+            int i = 0;
+            int j = N - 1;
+            while (i < j) {
+                if (A[i] + A[j] == find) {
+                    if (i != k && j != k) {
+                        cnt++;
+                        break;
+                    } else if (i == k) {
+                        i++;
+                    } else if (j == k) {
+                        j--;
+                    }
+                } else if (A[i] + A[j] < find) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        bf.close();
+    }
+}
