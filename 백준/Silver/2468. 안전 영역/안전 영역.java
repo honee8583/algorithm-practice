16d7af936e9f6328f43a2ddb77af9dec7ada1ee9
honee8583 <93666531+honee8583@users.noreply.github.com>
2025-07-21 09:51:24 +0900

    [Silver I] Title: 안전 영역, Time: 244 ms, Memory: 20040 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -12,19 +12,17 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        StringTokenizer st;
         arr = new int[N][N];
+        
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
                 max = Math.max(arr[i][j], max);
-                min = Math.min(arr[i][j], min);
             }
         }
 
@@ -37,7 +35,6 @@ class Main {
                 for (int j = 0; j < N; j++) {
                     if (!visited[i][j] && arr[i][j] > rain) {
                         DFS(i, j, rain);
-                        // BFS(i, j, rain);
                         cnt++;
                     }
                 }
@@ -66,30 +63,4 @@ class Main {
             }
         }
     }
-
-    // public static void BFS(int i, int j, int rain) {
-    //     Queue<int[]> q = new LinkedList<>();
-    //     q.add(new int[]{i, j});
-
-    //     while (!q.isEmpty()) {
-    //         int[] cur = q.poll();
-
-    //         int x = cur[0];
-    //         int y = cur[1];
-
-    //         if (!visited[x][y] && arr[x][y] > rain) {
-    //             visited[x][y] = true;
-    //         }
-
-    //         for (int k = 0; k < 4; k++) {
-    //             int nX = x + dx[k];
-    //             int nY = y + dy[k];
-    //             if (nX >= 0 && nX < N && nY >= 0 && nY < N) {
-    //                 if (!visited[nX][nY] && arr[nX][nY] > rain) {
-    //                     q.add(new int[]{nX, nY});
-    //                 }
-    //             }
-    //         }    
-    //     }
-    // }
 }
