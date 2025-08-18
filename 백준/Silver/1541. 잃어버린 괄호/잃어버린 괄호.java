708350a78598cd9a4575a8d1cc2d2a96a3f03c15
honee8583 <93666531+honee8583@users.noreply.github.com>
2025-08-19 08:54:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 172 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -2,47 +2,32 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        String A = sc.nextLine();
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+ 
+		int sum = Integer.MAX_VALUE;
+		String[] subtraction = sc.nextLine().split("-");
 
-        List<String> sArr = new ArrayList<>();
-        String[] arr = A.split("");
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            String cur = arr[i];
+        // 뺄셈 기준으로 split하게 되면 숫자, +를 포함 숫자로 나뉘게 됩니다.
+        // +를 포함한 숫자들은 같이 더해서 한번에 빼줍니다.
+        // 즉, split을 적절하게 잘 사용하면 쉽게 해결할 수 있습니다.
+		for(int i = 0; i < subtraction.length; i++) {
+			int temp = 0;
+ 
+			String[] addition = subtraction[i].split("\\+");
+			
+			for(int j = 0; j < addition.length; j++) {
+				temp += Integer.parseInt(addition[j]);
+			}
+			
+			// 첫 번째 숫자일 경우
+			if (sum == Integer.MAX_VALUE) {
+				sum = temp;
+			} else { // 그 다음 숫자일 경우
+				sum -= temp;
+			}
+		}
 
-            if (!cur.equals("+") && !cur.equals("-")) {
-                sb.append(cur);
-            } else {
-                sArr.add(sb.toString());
-                sArr.add(cur);
-                sb = new StringBuilder();
-            }
-        }
-        sArr.add(sb.toString());
-
-        // System.out.println(sArr.toString());
-
-        int sum = Integer.parseInt(sArr.get(0));
-        int tmp = 0;
-        for (int i = 1; i < sArr.size(); i+=2) {
-            String a = sArr.get(i);
-            int num = Integer.parseInt(sArr.get(i + 1));
-
-            if (a.equals("-")) {
-                sum -= tmp;
-                tmp = num;
-            } else {
-                if (tmp != 0) {
-                    tmp += num;
-                } else {
-                    sum += num;
-                }
-            }
-        }
-        sum -= tmp;
-
-        System.out.println(sum);
-    }
+		System.out.println(sum);
+	}
 }
