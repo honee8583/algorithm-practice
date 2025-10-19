11a2b55877e076e49a7dd25f7937664028c92c32
honee8583 <93666531+honee8583@users.noreply.github.com>
2025-10-19 20:57:10 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 940 ms, Memory: 280400 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -9,12 +9,12 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 도시의 개수
-        M = Integer.parseInt(st.nextToken()); // 도로의 개수
-        K = Integer.parseInt(st.nextToken()); // 목표 최단거리
-        X = Integer.parseInt(st.nextToken()); // 출발노드
+
+        N = Integer.parseInt(st.nextToken());  // 도시의 개수
+        M = Integer.parseInt(st.nextToken());  // 도로의 개수
+        K = Integer.parseInt(st.nextToken());  // 거리 정보
+        X = Integer.parseInt(st.nextToken());  // 출발지
 
         adj = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
@@ -23,33 +23,33 @@ class Main {
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            adj.get(a).add(b);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            adj.get(start).add(end);  // 단방향 그래프
         }
 
-        dist = new int[N + 1];
+        dist = new int[N + 1];  // 최소 거리 배열
         for (int i = 0; i <= N; i++) {
             dist[i] = Integer.MAX_VALUE;
         }
-        dist[X] = 0;
 
+        dist[X] = 0;
         BFS(X);
 
-        boolean hasAnswer = false;
+        boolean flag = false;
         for (int i = 1; i <= N; i++) {
             if (dist[i] == K) {
-                hasAnswer = true;
+                flag = true;
                 bw.write(i + "\n");
             }
         }
 
-        if (!hasAnswer) {
-            bw.write(-1 + "\n");
+        if (!flag) {
+            System.out.println(-1);
+        } else {
+            bw.flush();
         }
 
-        bw.flush();
         bw.close();
         br.close();
     }
@@ -58,12 +58,13 @@ class Main {
         Queue<Integer> q = new LinkedList<>();
         q.add(node);
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int cur = q.poll();
 
-            for (int next : adj.get(cur)) {
-                if (dist[cur] + 1 < dist[next]) {
-                    dist[next] = dist[cur] + 1;
+            for(int next : adj.get(cur)) {
+                int distance = dist[cur] + 1;
+                if (distance < dist[next]) {
+                    dist[next] = distance;
                     q.add(next);
                 }
             }
