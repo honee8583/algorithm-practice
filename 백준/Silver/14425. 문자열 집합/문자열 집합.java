f0e74a895a067a0620b08f49ee77ce6b5ffa4f84
honee8583 <93666531+honee8583@users.noreply.github.com>
2025-11-27 14:44:10 +0900

    [Silver IV] Title: 문자열 집합, Time: 2632 ms, Memory: 859712 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -3,14 +3,9 @@ import java.util.*;
 
 class Main {
 
-    private static class Node {
-        Node[] nodes;
-        boolean isLeaf;
-
-        public Node() {
-            this.nodes = new Node[26];
-            this.isLeaf = false;
-        }
+    private static class tNode {
+        tNode[] next = new tNode[26];
+        boolean isLeaf = false;
     }
 
     public static void main(String[] args) throws IOException {
@@ -19,43 +14,42 @@ class Main {
         int M = sc.nextInt();
         sc.nextLine();
 
-        // 트라이 자료구조 구성
-        Node trie = new Node();
+        tNode root = new tNode();
         for (int i = 0;i < N;i++) {
-            String str = sc.nextLine();
-            Node cur = trie;
-            for (int j = 0;j < str.length();j++) {
-                char c = str.charAt(j);
+            String target = sc.nextLine();
+            tNode cur = root;
+            for (int j = 0;j < target.length();j++) {
+                char c = target.charAt(j);
 
-                if (cur.nodes[c - 'a'] == null) {
-                    cur.nodes[c - 'a'] = new Node();
+                if (cur.next[c - 'a'] == null) {
+                    cur.next[c - 'a'] = new tNode();
                 }
 
-                if (j == str.length() - 1) {
-                    cur.nodes[c - 'a'].isLeaf = true;
+                if (j == target.length() - 1) {
+                    cur.next[c - 'a'].isLeaf = true;
                 }
 
-                cur = cur.nodes[c - 'a'];
+                cur = cur.next[c - 'a'];
             }
         }
 
         int cnt = 0;
         for (int i = 0;i < M;i++) {
-            Node cur = trie;
+            String target = sc.nextLine();
+            tNode cur = root;
+            for (int j = 0;j < target.length();j++) {
+                char c = target.charAt(j);
 
-            String str = sc.nextLine();
-            char[] cArr = str.toCharArray();
-            for (int j = 0;j < cArr.length;j++) {
-                int idx = cArr[j] - 'a';
-                cur = cur.nodes[idx]; 
-
-                if (cur == null) {
+                if (cur.next[c - 'a'] == null) {
                     break;
                 }
 
-                if (j == cArr.length - 1 && cur.isLeaf) {  // 마지막 문자 & 리프노드
+                if (j == target.length() - 1 && cur.next[c - 'a'].isLeaf) {
                     cnt++;
+                    break;
                 }
+
+                cur = cur.next[c - 'a'];
             }
         }
 
