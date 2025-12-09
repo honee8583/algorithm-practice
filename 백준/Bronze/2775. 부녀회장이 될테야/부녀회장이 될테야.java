ed05b4331f804b85944b93bb465b164fcc8301f1
honee8583 <93666531+honee8583@users.noreply.github.com>
2025-12-09 11:16:42 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 180 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -6,25 +6,22 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
 
+        int[][] dp = new int[15][15];
+        for (int i = 1;i < 15;i++) {
+            dp[0][i] = i;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 1;i < 15;i++) {
+            for (int j = 2;j < 15;j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+
+
         for (int t = 0;t < T;t++) {
             int K = sc.nextInt();  // k층
             int N = sc.nextInt();  // N호
-
-            int[][] dp = new int[K + 1][N + 1];
-            for (int i = 1;i <= N;i++) {
-                dp[0][i] = i;
-            }
-
-            for (int i = 1;i <= K;i++) {
-                for (int j = 1;j <= N;j++) {
-                    int sum = 0;
-                    for (int k = 1;k <= j;k++) {
-                        sum += dp[i - 1][k];
-                    }
-                    dp[i][j] = sum;
-                }
-            }
-
             System.out.println(dp[K][N]);
         }
     }
