6d1066d5bb695e12128ea60f4891fb460ac19278
honee8583 <93666531+honee8583@users.noreply.github.com>
2026-01-03 14:01:49 +0900

    [Gold V] Title: 집합의 표현, Time: 948 ms, Memory: 54024 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,51 +2,53 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int N, M;
     static int[] parent;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         parent = new int[N + 1];
-        for (int i = 1;i <= N; i++) {
+        for (int i = 1;i <= N;i++) {
             parent[i] = i;
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0;i < M;i++) {
             st = new StringTokenizer(br.readLine());
-            int cal = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            if (cal == 0) {
-                union(a, b);
+            if (a == 0) {
+                union(b, c);
             } else {
-                if (find(a) == find(b)) {
+                b = find(b);
+                c = find(c);
+                if (b == c) {
                     System.out.println("YES");
                 } else {
                     System.out.println("NO");
                 }
             }
         }
-
-        System.out.println(Arrays.toString(parent));
     }
 
-    public static void union(int a, int b) {
-        int pa = find(a);
-        int pb = find(b);
-        if (pa != pb) {
-            parent[pa] = pb;
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[a] = b;
         }
     }
 
-    public static int find(int node) {
-        if (parent[node] == node) {
+    private static int find(int node) {
+        if (node == parent[node]) {
             return node;
         }
         return parent[node] = find(parent[node]);
     }
+    
 }
