23b49e903f6ab30ca71ba4f74dc962d4936014b3
honee8583 <93666531+honee8583@users.noreply.github.com>
2026-01-05 14:11:15 +0900

    [Gold III] Title: 줄 세우기, Time: 720 ms, Memory: 54448 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -2,55 +2,54 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static List<List<Integer>> adj;
-    static int[] arr;
     static int N, M;
+    static List<List<Integer>> adj;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = parse(st.nextToken());
-        M = parse(st.nextToken());
+        N = parseInt(st.nextToken());
+        M = parseInt(st.nextToken());
 
         adj = new ArrayList<>();
         for (int i = 0;i <= N;i++) {
             adj.add(new ArrayList<>());
         }
-
-        // 인접리스트 및 진입차수 배열 구성
-        arr = new int[N + 1];
+        
+        int[] indegree = new int[N + 1];
         for (int i = 0;i < M;i++) {
             st = new StringTokenizer(br.readLine());
-            int a = parse(st.nextToken());
-            int b = parse(st.nextToken());
-
-            adj.get(a).add(b);
-            arr[b] += 1;
+            int A = parseInt(st.nextToken());
+            int B = parseInt(st.nextToken());
+            adj.get(A).add(B);
+            indegree[B]++;
         }
 
-        // 진입차수가 0인 노드를 선택
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 1; i <= N;i++) {
-            if (arr[i] == 0) {
+        for (int i = 1;i <= N;i++) {
+            if (indegree[i] == 0) {
                 q.offer(i);
             }
         }
 
-        // 진입차수가 0인 노드의 인접노드의 진입차수 - 1
+        List<Integer> result = new ArrayList<>();
         while (!q.isEmpty()) {
             int cur = q.poll();
-            System.out.print(cur + " ");
+            result.add(cur);
 
             for (int next : adj.get(cur)) {
-                arr[next]--;
-                if (arr[next] == 0) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
                     q.offer(next);
                 }
             }
         }
+
+        for (int r : result) {
+            System.out.print(r + " ");
+        }
     }
 
-    public static Integer parse(String num) {
+    public static Integer parseInt(String num) {
         return Integer.parseInt(num);
     }
 }
