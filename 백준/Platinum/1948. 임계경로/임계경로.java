9ab679d17ffd2bf61dd6aa84fd1758f80cb81415
honee8583 <93666531+honee8583@users.noreply.github.com>
2026-01-11 14:27:45 +0900

    [Platinum V] Title: 임계경로, Time: 380 ms, Memory: 49888 KB -BaekjoonHub

--- 백준/Platinum/1948. 임계경로/임계경로.java
+++ 백준/Platinum/1948. 임계경로/임계경로.java
@@ -2,106 +2,92 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int N, M, S, E;
-    static List<List<Integer>> adj, rAdj;
-    static int[][] times;
+    static int N, M, start, end, cnt;
     static int[] indegree;
-    static int[] cp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = parse(br.readLine());
-        M = parse(br.readLine());
+    static long[] dist;
+    static boolean[] visited;
+    static List<List<Node>> adj, rAdj;
 
-        adj = new ArrayList<>();  // 정방향
-        rAdj = new ArrayList<>(); // 역방향
+    private static class Node {
+        int v;
+        int w;
+
+        public Node (int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        adj = new ArrayList<>();
+        rAdj = new ArrayList<>();
         for (int i = 0;i <= N;i++) {
             adj.add(new ArrayList<>());
             rAdj.add(new ArrayList<>());
         }
 
-        indegree = new int[N + 1];  // 진입차수 배열
-        times = new int[N + 1][N + 1];
+        StringTokenizer st;
+        indegree = new int[N + 1];
         for (int i = 0;i < M;i++) {
             st = new StringTokenizer(br.readLine());
-
-            int start = parse(st.nextToken());
-            int end = parse(st.nextToken());
-            int time = parse(st.nextToken());
-
-            // 시간배열
-            times[start][end] = time;
-
-            // 인접리스트
-            adj.get(start).add(end);
-            
-            // 역방향 인접리스트
-            rAdj.get(end).add(start);
-
-            indegree[end]++;
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adj.get(u).add(new Node(v, w));
+            rAdj.get(v).add(new Node(u, w));
+            indegree[v]++;
         }
 
         st = new StringTokenizer(br.readLine());
-        S = parse(st.nextToken());
-        E = parse(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
 
-        int time = getResult();
-        int cnt = reverseSearch();
+        dist = new long[N + 1];
+        getCriticalPath();
 
-        System.out.println(time);
+        cnt = 0;
+        visited = new boolean[N + 1];
+        visited[end] = true;
+        reverseDfs(end);
+
+        System.out.println(dist[end]);
         System.out.println(cnt);
-
     }
 
-    private static int getResult() {
-        cp = new int[N + 1];
+    private static void getCriticalPath() {
         Queue<Integer> q = new LinkedList<>();
         for (int i = 1;i <= N;i++) {
             if (indegree[i] == 0) {
-                q.add(i);
+                q.offer(i);
             }
         }
 
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            for (int next : adj.get(node)) {
-                cp[next] = Math.max(cp[next], cp[node] + times[node][next]);
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    q.add(next);
-                }
-            }
-        }
-
-        return cp[E];
-    }
-
-    private static int reverseSearch() {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(E);
-
-        boolean[] visited = new boolean[N + 1];
-        visited[E] = true;
-
-        int cnt = 0;
         while (!q.isEmpty()) {
             int cur = q.poll();
-            for (int next : rAdj.get(cur)) {
-                if (cp[cur] == cp[next] + times[next][cur]) {
-                    cnt++;
-                    if (!visited[next]) {
-                        q.add(next);
-                        visited[next] = true;
-                    }
+            for (Node next : adj.get(cur)) {
+                dist[next.v] = Math.max(dist[next.v], dist[cur] + next.w);
+                indegree[next.v]--;
+                if (indegree[next.v] == 0) {
+                    q.offer(next.v);
                 }
             }
         }
-
-        return cnt;
     }
 
-    private static int parse(String token) {
-        return Integer.parseInt(token);
+    private static void reverseDfs(int cur) {
+        for (Node next : rAdj.get(cur)) {
+            int v = next.v;
+            if (dist[v] + next.w == dist[cur]) {
+                cnt++;
+                if (!visited[v]) {
+                    visited[v] = true;
+                    reverseDfs(v);
+                }
+            }
+        }
     }
 }
