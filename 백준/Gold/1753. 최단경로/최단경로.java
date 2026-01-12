b1c41a40c921ca75d568752ed84934a24621277d
honee8583 <93666531+honee8583@users.noreply.github.com>
2026-01-12 16:13:40 +0900

    [Gold IV] Title: 최단경로, Time: 840 ms, Memory: 110628 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,93 +2,68 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int V, E, K;
+    static long[] dist;
+    static boolean[] visited;
+    static List<List<Node>> adj;
+    private static class Node {
+        int v;
+        int w;
 
-    static class Node implements Comparable<Node> {
-        public int next;
-        public int weight;
-
-        public Node(int next, int weight) {
-            this.next = next;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Node node) {
-            return this.weight - node.weight;
+        Node(int v, int w) {
+            this.v = v;
+            this.w = w;
         }
     }
 
-    static int V, E, K;
-    static int[] dist;
-    static boolean[] visited;
-    static List<List<Node>> adj;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        V = parse(st.nextToken());
-        E = parse(st.nextToken());
-        K = parse(br.readLine());
+        V = Integer.parseInt(st.nextToken());  // 정점 수
+        E = Integer.parseInt(st.nextToken());  // 간선 수
+        K = Integer.parseInt(br.readLine());   // 시작 정점
 
         adj = new ArrayList<>();
         for (int i = 0;i <= V;i++) {
             adj.add(new ArrayList<>());
         }
-
-        // 인접리스트 구성
-        for (int i = 0; i < E; i++) {
+        
+        for (int i = 0;i < E;i++) {
             st = new StringTokenizer(br.readLine());
-            int u = parse(st.nextToken());
-            int v = parse(st.nextToken());
-            int w = parse(st.nextToken());
-
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
             adj.get(u).add(new Node(v, w));
         }
 
-        // 최단배열 구성
-        // 시작노드는 0으로 설정
-        // 나머지는 무한대로 설정
-        dist = new int[V + 1];
+        dist = new long[V + 1];
+        Arrays.fill(dist, Long.MAX_VALUE);
         dist[K] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((x, y) -> x.w - y.w);
+        pq.offer(new Node(K, 0));
+
         visited = new boolean[V + 1];
-        for (int i = 1;i <= V; i++) {
-            if (i != K) {
-                dist[i] = Integer.MAX_VALUE;
-            }
-        }
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if (visited[cur.v]) continue;
+            visited[cur.v] = true;
 
-        
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(K, 0));
-
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            int cur = node.next;
-            if (visited[cur]) {  // K, 0
-                continue;
-            }
-            visited[cur] = true; // K = true
-
-            for (Node nextNode : adj.get(cur)) {  // (2, 2), (3, 3)
-                int next = nextNode.next;  // 다음 노드 번호
-                int weight = nextNode.weight;  // 다음 노드로의 가중치
-                dist[next] = Math.min(dist[next], dist[cur] + weight);
-                if (!visited[next]) {
-                    q.add(new Node(next, dist[next]));
+            for (Node next : adj.get(cur.v)) {
+                if (dist[next.v] > dist[cur.v] + next.w) {
+                    dist[next.v] = dist[cur.v] + next.w;
+                    pq.offer(new Node(next.v, (int) dist[next.v]));
                 }
             }
         }
 
         for (int i = 1;i <= V;i++) {
-            if (dist[i] != Integer.MAX_VALUE) {
-                System.out.println(dist[i]);
-            } else {
+            if (!visited[i]) {
                 System.out.println("INF");
+            } else {
+                System.out.println(dist[i]);
             }
         }
-    }
 
-    public static Integer parse(String token) {
-        return Integer.parseInt(token);
     }
 }
